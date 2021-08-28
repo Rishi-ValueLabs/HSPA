df4b335e624c04ec71618f3388c64c2ce92b2659
Rishi-ValueLabs <rishiraj.malvi@gmail.com>
2021-08-28 17:44:58 +0530

    Code commit with changes

--- Frontend/src/app/property/property-list/property-list.component.ts
+++ Frontend/src/app/property/property-list/property-list.component.ts
@@ -1,4 +1,8 @@
+
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { HousingService } from 'src/app/services/housing.service';
+import { IProperty } from '../IProperty';
 
 @Component({
   selector: 'app-property-list',
@@ -7,9 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PropertyListComponent implements OnInit {
 
-  constructor() { }
+  // Array defined in javascript
+  properties : IProperty[];
+
+  SellRent = 1;
+
+  constructor(private route :ActivatedRoute, private housingService : HousingService) { }
 
   ngOnInit(): void {
+
+    if(this.route.snapshot.url.toString())
+    {
+      this.SellRent = 2; // Means Rent property
+    }
+
+    this.housingService.getAllProperties(this.SellRent).subscribe(data=>{
+      this.properties = data;
+      console.log(this.route.snapshot.url.toString());
+    },error=>{
+      console.log(error);
+    })
+    // this.http.get('data/properties.json').subscribe(
+    //   data =>{
+    //     this.properties = data;
+    //     console.log(data)}
+    //   );
   }
 
 }
